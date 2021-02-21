59211e95a8c7301c0373132ef3dfa3b280f7f703
denimnani <49691619+denimnani@users.noreply.github.com>
2021-02-21 16:41:40 +0000

    Update OSserver.py

--- resources/lib/OSserver.py
+++ resources/lib/OSserver.py
@@ -1,48 +1,53 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import os, sys, xbmc, xmlrpclib
-from utils import name, version, setting, boolsetting
+import os, sys, xbmc
+from resources.lib.utils import name, version, setting, boolsetting
+
+try:
+    import xmlrpc.client as xc
+except:
+    import xmlrpclib
+    xc = xmlrpclib
+
+
+
+OpenSubtitles = xc.Server('http://api.opensubtitles.org/xml-rpc', verbose=0)
 
 
 
 
-OpenSubtitles = xmlrpclib.Server('http://api.opensubtitles.org/xml-rpc', verbose=0)
-
-
-
-
-#OpenSubtilte.org functions******************************************************************************************************************************
+#OpenSubtilte.org functions*******************************************************************************************************************************
 
 def OSserver():
-	user = ""
-	password = ""
-	ua = "kodi_%s_v%s" % (name().lower(), version())
-	if boolsetting('OScustomuser'):
-		if setting ('OSuser') != '' or setting('OSpassword') != '':
-			user = setting('OSuser')
-			password = setting('OSpassword')
-		else:
-			user = "error"
-			password = "error"
-	return OpenSubtitles.LogIn(user, password, 'en', ua)
+        user = ""
+        password = ""
+        ua = "kodi_%s_v%s" % (name().lower(), version())
+        if boolsetting('OScustomuser'):
+                if setting ('OSuser') != '' or setting('OSpassword') != '':
+                        user = setting('OSuser')
+                        password = setting('OSpassword')
+                else:
+                        user = "error"
+                        password = "error"
+        return OpenSubtitles.LogIn(user, password, 'en', ua)
 
 
 def OSinfo():
-	return OpenSubtitles.ServerInfo()['website_url']
+        return OpenSubtitles.ServerInfo()['website_url']
 
 
 def OStoken():
-	return OSserver()['token']
+        return OSserver()['token']
 
-	
+
 def OSuser():
-	return OSserver()['status'] == "200 OK"
+        return OSserver()['status'] == "200 OK"
 
 
 def OSusersetting():
-	if boolsetting('OScustomuser') and (setting('OSuser') != setting('OSusercheck') or setting('OSpassword') != setting('OSpasswordcheck')):
-		return True
-	return False
+        if boolsetting('OScustomuser') and (setting('OSuser') != setting('OSusercheck') or setting('OSpassword') != setting('OSpasswordcheck')):
+                return True
+        return False
 
 #*********************************************************************************************************************************************************
